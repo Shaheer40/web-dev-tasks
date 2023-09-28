19ffbe1a5b4c72541e97f4050a2b26aa3d1f9520
Shaheer40 <140733754+Shaheer40@users.noreply.github.com>
2023-09-28 17:39:27 +0500

    Add files via upload
    task2 (Quiz WebApp)

---
+++ questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which planet has the most moons?",
+    answer: "Saturn",
+    options: [
+      "Jupiter",
+      "Uranus",
+      "Saturn",
+      "Neptune"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many bones do we have in an ear?",
+    answer: "3",
+    options: [
+      "1",
+      "3",
+      "5",
+      "7"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many hearts does an octopus have? ",
+    answer: "3",
+    options: [
+      "3",
+      "1",
+      "2",
+      "5"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How many stars are on the Chinese flag?",
+    answer: "5",
+    options: [
+      "2",
+      "3",
+      "4",
+      "5"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What colors is the flag of the United Nations?",
+    answer: "Blue and White",
+    options: [
+      "Blue and Red",
+      "Red and White",
+      "Blue and White",
+      "Green and White"
+    ]
+  },
+];
